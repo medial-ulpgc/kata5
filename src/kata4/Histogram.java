43a7b41d83278a7366659a638c929768266bb7ce
Alejandro Medina <alejandro.medina104@alu.ulpgc.es>
2020-11-11 06:03:15 +0000

    renaming project to kata4

--- src/kata4/Histogram.java
+++ src/kata4/Histogram.java
@@ -1,4 +1,4 @@
-package kata3;
+package kata4;
 
 import java.util.HashMap;
 import java.util.Map;
--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,8 +1,8 @@
-package kata3;
+package kata4;
 
 import java.util.Arrays;
 
-public class Kata3 {
+public class Kata4 {
 
 
     public static void main(String[] args) {
